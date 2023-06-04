50d6fdd07be8db6fec678109b0c4bc7184a997c0
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-06-05 01:30:12 +0300

    add increase,decrease,remove,clear cart functions

--- src/context.jsx
+++ src/context.jsx
@@ -8,19 +8,38 @@ import {
   LOADING,
   DISPLAY_ITEMS,
 } from "./actions";
+import cartItems from "./data";
 
 const AppContext = createContext();
 
 const initialState = {
   loading: false,
-  cart: [],
+  cart: new Map(cartItems.map((item) => [item.id, item])),
 };
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
+
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE, payload: { id } });
+  };
+  const increaseItem = (id) => {
+    dispatch({ type: INCREASE, payload: { id } });
+  };
+  const decreaseItem = (id) => {
+    dispatch({ type: DECREASE, payload: { id } });
+  };
+
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider
+      value={{ ...state, clearCart, removeItem, increaseItem, decreaseItem }}
+    >
+      {children}
+    </AppContext.Provider>
   );
 };
 
